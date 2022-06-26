74ed1a5601ef118a5998b76b4e5f80613d085ee7
Justin Marshall <justinmarkmarshall1@gmail.com>
2022-06-26 14:09:01 +0100

    add the query and GQL server

--- CommanderGQL/Program.cs
+++ CommanderGQL/Program.cs
@@ -1,12 +1,18 @@
 using CommanderGQL.Data;
+using CommanderGQL.GraphQL;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("CommandConStr");
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(connectionString))
-.AddScoped<DbContext, AppDbContext>();
+.AddScoped<DbContext, AppDbContext>()
+.AddGraphQLServer()
+.AddQueryType<Query>();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseRouting();
+app.UseEndpoints(endpoints => {
+    endpoints.MapGraphQL();
+});
 
 app.Run();
